c7a423fac8c74b3c0940eef9309470ad2ce956ee
Thomas Hetfeld <thomas@hetfeld.email>
2021-03-14 12:05:17 +0100

    Ticket Controller sends Comments in addition to ticket

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -17,4 +17,19 @@ namespace ticketsystem_backend.Models
         public User LastChangedBy { get; set; }
         public DateTime LastChangedDate { get; set; }
     }
+
+
+    public class TicketVM
+    {
+        public int Id { get; set; }
+        public Document Document { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool TicketClosed { get; set; }
+        public User CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public User LastChangedBy { get; set; }
+        public DateTime LastChangedDate { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
 }
